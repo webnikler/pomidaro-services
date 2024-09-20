f89d6dcaf29ab325d10435fbaf8cfc6a9c9dd415
Никита Петров <n.petrov@yclients.tech>
2024-09-20 19:07:09 +0300

    feat(uikit-web/components): init Layout

--- libs/uikit-web/src/index.ts
+++ libs/uikit-web/src/index.ts
@@ -1,1 +1,2 @@
+export * from './lib/components/Layout';
 export * from './lib/uikit-web';
---
+++ libs/uikit-web/src/lib/components/Layout.tsx
@@ -0,0 +1,11 @@
+import styles from './Layout.module.scss';
+
+export function Layout() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Layout!</h1>
+    </div>
+  );
+}
+
+export default Layout;
