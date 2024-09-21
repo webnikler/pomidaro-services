1cf4b31545d087307aad868cfd272f18afb5770a
webnikler <webnikler@gmail.com>
2024-09-21 15:26:17 +0300

    chore(*): add viewports for storybook

--- libs/uikit-web/.storybook/preview.ts
+++ libs/uikit-web/.storybook/preview.ts
@@ -1,9 +1,13 @@
 import { Preview } from '@storybook/react';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 const preview: Preview = {
   parameters: {
     backgrounds: {
       default: 'dark',
+    },
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
     },
   },
 };
